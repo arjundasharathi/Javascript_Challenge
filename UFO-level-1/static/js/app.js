d58b2cf1456b9b01ae95b259775a2531ca5ed0bb
arjundasharathi <arjun.dasharathi@icloud.com>
2020-02-17 16:28:15 -0500

    .

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,30 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function buildtable(xdata){
+    tbody.html("");
+
+    xdata.forEach(record => {
+        var row =  tbody.append("tr");
+        
+        Object.entries(record).forEach(([key,value]) => { 
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    })
+
+}
+
+function filtertable(){
+    var filtered = tableData;
+    var userinput = d3.select("#datetime").property("value");
+    filtered = tableData.filter(info => info.datetime === userinput);
+    buildtable(filtered);
+
+}
+
+var button = d3.select("#filter-btn");
+button.on("click",filtertable);
+
